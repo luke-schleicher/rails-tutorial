ed645c4487e934b1f08ef8613a8f0538cee808d0
luke-schleicher <luke.j.schleicher@gmail.com>
2016-11-21 12:21:57 -0600

    Added home, about, and help pages and tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,11 @@
 Rails.application.routes.draw do
 
   get 'static_pages/home'
-
   get 'static_pages/help'
+  get 'static_pages/about'
+  root 'application#hello'
 
-  Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
-    root 'application#hello'
-  end
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -65,4 +60,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
